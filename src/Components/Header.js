5ca4ebce22515cc6cd40c828c3e841d251102996
n-hari159 <n.hari159@gmail.com>
2019-03-05 09:48:27 +0530

    Header Component Included

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return ( <
+        div >
+        <
+        h1 > I 'm Header Component</h1> < /
+        div >
+    )
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Components/Header";
+
+//Components
+
+const App = () => {
+    return ( <
+        div >
+        <
+        Header / >
+        <
+        /div>
+    )
+}
+ReactDOM.render( < App / > , document.querySelector("#root"));
