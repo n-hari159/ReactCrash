87bd602c726126b53a0cd437136282691637de7c
n-hari159 <n.hari159@gmail.com>
2019-03-05 11:29:02 +0530

    Added React event=> onclick

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
-import CSS from "../CSS/style.css";
+import "../CSS/style.css";
 
 class Header extends React.Component{
+    onClickHandler(){
+        console.log("I just Clicked");
+    }
     render(){
         return (
             <header>
             <div className="logo"> Logo </div>
-            <input type="text"></input>
+            <input onClick={this.onClickHandler} type="text" />
             </header>
         )
     }
