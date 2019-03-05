4f85a4305f27ad2c20d17a48527535df81067b7c
n-hari159 <n.hari159@gmail.com>
2019-03-05 12:21:12 +0530

    Set State

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,23 @@ import React from "react";
 import "../CSS/style.css";
 
 class Header extends React.Component{
-    onClickHandler(){
-        console.log("I just Clicked");
+    constructor(props){
+        super(props);
+        this.state={
+            title:""
+        }
+    }
+    onChangeHandler(e){
+        this.setState({
+            title:e.target.value
+        })
     }
     render(){
         return (
             <header>
             <div className="logo"> Logo </div>
-            <input onClick={this.onClickHandler} type="text" />
+            <input onChange={this.onChangeHandler.bind(this)} type="text" />
+            <div> {this.state.title} </div>
             </header>
         )
     }
