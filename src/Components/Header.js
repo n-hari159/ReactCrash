aedf1043e0cb8c2d85450c41d53f65ded83f653b
n-hari159 <n.hari159@gmail.com>
2019-03-05 10:53:05 +0530

    Creating functional component to class component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,13 @@
 import React from "react";
 
-const info={
-    fname:"Hari",
-    lname:"Nakka"
-}
-
-const year=()=>{
-    const date=new Date();
-    return date.getFullYear();
-}
-
-const Header = () => {
-    return (
-        <div>
-        <h1> Hello {info.fname} {year()}</h1>
-        </div>
-    )
+class Header extends React.Component{
+    render(){
+        return (
+            <div>
+            <h1> Hello </h1>
+            </div>
+        )
+    }
 }
 
 export default Header;
